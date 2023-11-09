a32aee5b5445f8952797c44729b5eeda441dfacf
mk812y <mk812y@gmail.com>
2023-11-09 15:04:42 +0100

    Initial Commit

---
+++ twenty five plus five/twenty_five_plus_fiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  twenty_five_plus_fiveApp.swift
+//  twenty five plus five
+//
+//  Created by Михаил Куприянов on 9.11.23..
+//
+
+import SwiftUI
+
+@main
+struct twenty_five_plus_fiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
